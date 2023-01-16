2612a4d02a8dd33e156ae0acbca2c932552beb81
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-01-16 13:01:17 +0100

    FIX invalid HTML in files upload dialog

--- Atheos/components/transfer/dialog.php
+++ Atheos/components/transfer/dialog.php
@@ -56,7 +56,7 @@ switch ($action) {
 			<pre><?php echo($path); ?></pre>
 			<label id="upload_wrapper">
 				<?php echo i18n("dragFilesOrClickHereToUpload"); ?>
-				<input type="hidden" name=“MAX_FILE_SIZE” value="<?php echo max_file_upload_in_bytes(); ?>">
+				<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo max_file_upload_in_bytes(); ?>">
 				<input class="hidden" type="file" name="upload[]" multiple>
 			</label>
 			<div id="progress_wrapper">
