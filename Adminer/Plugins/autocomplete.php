f8bab12da43f9c1b08adc98efe59a771ba340633
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-03-03 11:43:02 +0100

    NEW SQL autocomplete in Adminer now supports table aliases

--- Adminer/Plugins/autocomplete.php
+++ Adminer/Plugins/autocomplete.php
@@ -8,8 +8,9 @@
 class AdminerAutocomplete
 {
 	public $keywords = [
-		'DELETE FROM', 'DISTINCT', 'EXPLAIN', 'FROM', 'GROUP BY', 'HAVING', 'INSERT INTO', 'INNER JOIN', 'IGNORE',
-		'LIMIT', 'LEFT JOIN', 'NULL', 'ORDER BY', 'ON DUPLICATE KEY UPDATE', 'SELECT', 'UPDATE', 'WHERE',
+		'AS', 'DELETE FROM', 'DISTINCT', 'EXPLAIN', 'FROM', 'GROUP BY', 'HAVING', 'INSERT INTO', 'INNER JOIN', 'IGNORE',
+		'LIMIT', 'LEFT JOIN', 'NULL', 'ORDER BY', 'ON DUPLICATE KEY UPDATE', 'SELECT', 'UPDATE', 'WHERE', 'ON', 'USING',
+	    'CROSS_JOIN'
 	];
 
 
@@ -21,7 +22,7 @@ class AdminerAutocomplete
 
 		$suggests = [];
 		foreach (array_keys(tables_list()) as $table) {
-			$suggests[] = $table;
+			$suggests[] = trim($table);
 			foreach (fields($table) as $field => $foo) {
 				$suggests[] = "$table.$field";
 			}
@@ -52,13 +53,49 @@ document.addEventListener('DOMContentLoaded', () => {
 	editor.setOptions({
 		fontSize: 14,
 		enableBasicAutocompletion: [{
+			// note, won't fire if caret is at a word that does not have these letters
 			identifierRegexps: [/[a-zA-Z_0-9\.\-\u00A2-\uFFFF]/], // added dot
 			getCompletions: (editor, session, pos, prefix, callback) => {
-				// note, won't fire if caret is at a word that does not have these letters
-				callback(null, [
+				var oDoc = session.getDocument();
+				var sValue = oDoc.getValue();
+				var aSuggestions = [
 					...keywords.map((word) => ({value: word + ' ', score: 1, meta: 'keyword'})),
-					...suggests.map((word) => ({value: word + ' ', score: 2, meta: 'name'}))
-				]);
+					...suggests.map((word) => ({value: word, score: 2, meta: 'name'}))
+				];
+				
+				// Add table aliases from the current text 
+				aMatches = [...sValue.matchAll(/(from|join)\s+(\w+\.)?((\w*) (as )?(\w+))/gmi)];
+				aMatches.forEach(function(aMatch){
+					/*
+					[
+						0: [
+        					0: "FROM dbo.date_dimension AS dd",
+                            1: "FROM",
+                            2: "dbo."
+                            3: "date_dimension AS dd",
+                            4: "date_dimension",
+                            5: "AS"
+                            6: "dd",
+                        ],
+                    ]
+                    */
+					var sTable = aMatch[4];
+					var sAlias = aMatch[6];
+					var sOp = aMatch[1];
+					if ((sAlias.toUpperCase() === 'ON' || sAlias.toUpperCase() === 'USING') && sOp.toUpperCase() === 'JOIN') {
+						return;
+					}
+					if (sTable !== undefined && sAlias !== undefined) {
+						aSuggestions.push({value: sAlias, score: 3, meta: 'name'});
+						suggests.forEach(function(sWord){
+							if (sWord.startsWith(sTable+'.')) {
+								aSuggestions.push({value: sAlias + '.' + sWord.substring((sTable + '.').length), score: 3, meta: 'name'});
+							}
+						});
+					}
+				});
+				
+				callback(null, aSuggestions);
 			},
 		}],
 		// to make popup appear automatically, without explicit ctrl+space
